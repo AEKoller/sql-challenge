ee7be249bed3f1f76830befeec8b08fa6991616e
AEKoller <andrew@andrewkoller.org>
2024-01-12 20:56:24 -0600

    Completed

--- module_9_challenge.sql
+++ module_9_challenge.sql
@@ -3,66 +3,79 @@
 ----------------------------------------------------------------------------------- 
 ----------------------------------------------------------------------------------- 
 
---Part 1: creating tables
+--Part 1: Table Creation
 
 ----------------------------------------------------------------------------------- 
 
+DROP TABLE departments 
 --Table 'departments'
 CREATE TABLE departments (
-	dept_no VARCHAR(255)
-	,dept_name VARCHAR(255)
+	dept_no VARCHAR(255) NOT NULL PRIMARY KEY
+	,dept_name VARCHAR(255) NOT NULL UNIQUE
 );
 SELECT * FROM departments;
+----------------------------------------------------------------------------------- 
+
+DROP TABLE titles
+--Table 'titles'
+CREATE TABLE titles (
+	title_id VARCHAR(255) NOT NULL PRIMARY KEY
+	,title VARCHAR(255) NOT NULL
+);
+SELECT * FROM titles;
+
+----------------------------------------------------------------------------------- 
+
+DROP TABLE employees
+--Table 'employees'
+CREATE TABLE employees (
+	emp_no INT NOT NULL PRIMARY KEY
+	,emp_title_id VARCHAR(255) NOT NULL REFERENCES titles(title_id)
+	,birth_date VARCHAR(255) NOT NULL
+	,first_name VARCHAR(255) NOT NULL
+	,last_name VARCHAR(255) NOT NULL
+	,sex VARCHAR(255) NOT NULL
+	,hire_date DATE NOT NULL
+	,FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+SELECT * FROM employees;
 
 -----------------------------------------------------------------------------------
 
+DROP TABLE dept_emp
 --Table 'dept_emp'
 CREATE TABLE dept_emp (
-	emp_no INT 
-	,dept_no VARCHAR(255) 
+	emp_no INT NOT NULL
+	,dept_no VARCHAR(255) NOT NULL
+	,PRIMARY KEY (emp_no, dept_no)
+	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 SELECT * FROM dept_emp;
 
 ----------------------------------------------------------------------------------- 
 
+DROP TABLE dept_manager
 --Table 'dept_manager'
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(255) 
-	,emp_no INT 
+	dept_no VARCHAR(255) NOT NULL
+	,emp_no INT NOT NULL
+	,PRIMARY KEY (dept_no, emp_no)
+    ,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    ,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 SELECT * FROM dept_manager;
 
 ----------------------------------------------------------------------------------- 
-DROP TABLE employees
---Table 'employees'
-CREATE TABLE employees (
-	emp_no INT 
-	,emp_title_id VARCHAR(255) 
-	,birth_date VARCHAR(255)
-	,first_name VARCHAR(255)
-	,last_name VARCHAR(255)
-	,sex VARCHAR(255)
-	,hire_date DATE
-);
-SELECT * FROM employees;
 
------------------------------------------------------------------------------------ 
-
+DROP TABLE salaries
 --Table 'salaries'
 CREATE TABLE salaries (
-	emp_no INT
-	,salary INT
+	emp_no INT NOT NULL
+	,salary INT NOT NULL
+	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 SELECT * FROM salaries;
-
------------------------------------------------------------------------------------ 
-
---Table 'titles'
-CREATE TABLE titles (
-	title_id VARCHAR(255) 
-	,title VARCHAR(255)
-);
-SELECT * FROM titles;
 
 -----------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------- 
@@ -145,14 +158,55 @@ SELECT
 	,d.dept_name
 FROM dept_emp AS dem
 JOIN employees AS e ON (dem.emp_no=e.emp_no)
-JOIN departments as d ON dem.dept_no = d.dept_no
+JOIN departments as d ON dem.dept_no = d.dept_no;
 	
 ----------------------------------------------------------------------------------- 
 
 -- 5. List first name, last name, and sex of each employee whose first name is 
 --    Hercules and whose last name begins with the letter B.
+SELECT
+	e.first_name
+	,e.last_name
+	,e.sex
+FROM employees as e
+WHERE e.first_name = 'Hercules' and e.last_name LIKE 'B%';
 
+----------------------------------------------------------------------------------- 
 
+-- 6. List each employee in the Sales department, including their employee number, 
+--    last name, and first name.
+SELECT
+	dem.dept_no
+	,dem.emp_no
+	,e.first_name
+	,e.last_name
+FROM dept_emp as dem
+JOIN employees as e ON dem.emp_no = e.emp_no
+WHERE dem.dept_no = 'd007';
 
+----------------------------------------------------------------------------------- 
 
+-- 7. List each employee in the Sales and Development departments, including their 
+--    employee number, last name, first name, and department name.
+SELECT
+	dem.dept_no
+	,dem.emp_no
+	,e.first_name
+	,e.last_name
+	,d.dept_name
+FROM dept_emp as dem
+JOIN employees as e ON dem.emp_no = e.emp_no
+JOIN departments as d ON dem.dept_no = d.dept_no
+WHERE dem.dept_no = 'd007' OR dem.dept_no = 'd005'
+
+----------------------------------------------------------------------------------- 
+
+-- 8. List the frequency counts, in descending order, of all the employee last names 
+-- (that is, how many employees share each last name).
+SELECT 
+	e.last_name
+	,COUNT(e.last_name) as freq
+FROM employees as e
+GROUP BY e.last_name
+ORDER BY freq DESC
 
